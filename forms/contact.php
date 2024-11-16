1f7df0f5a28b8e3ed04da70663e569e74df6139c
kylin1209 <144865190+kylin1209@users.noreply.github.com>
2024-11-16 13:58:19 -0500

    committed website

---
+++ forms/contact.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = "linkylie05@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
